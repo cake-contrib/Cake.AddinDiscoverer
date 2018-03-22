6e64b9f095bbf733dfd3d921141d732860f09d3e
Jeremie Desautels <desautelsj@hotmail.com>
2018-03-21 21:17:18 -0400

    Formatting

--- Source/Cake.AddinDiscoverer/Extensions.cs
+++ Source/Cake.AddinDiscoverer/Extensions.cs
@@ -9,14 +9,14 @@ namespace Cake.AddinDiscoverer
 	{
 		public static string WithRightPadding(this string content, int desiredLength)
 		{
-			var count = Math.Max(0, desiredLength - content.Length);
-			return content + new string(' ', count);
+			var padLength = Math.Max(0, desiredLength - content.Length);
+			return content + new string(' ', padLength);
 		}
 
-		public static async Task<TResult[]> ForEachAsync<T, TResult>(this IEnumerable<T> items, Func<T, Task<TResult>> action, int maxThreads)
+		public static async Task<TResult[]> ForEachAsync<T, TResult>(this IEnumerable<T> items, Func<T, Task<TResult>> action, int maxDegreeOfParalellism)
 		{
 			var allTasks = new List<Task<TResult>>();
-			var throttler = new SemaphoreSlim(initialCount: maxThreads);
+			var throttler = new SemaphoreSlim(initialCount: maxDegreeOfParalellism);
 			foreach (var item in items)
 			{
 				await throttler.WaitAsync();
@@ -33,10 +33,10 @@ namespace Cake.AddinDiscoverer
 			return results;
 		}
 
-		public static async Task ForEachAsync<T>(this IEnumerable<T> items, Func<T, Task> action, int maxThreads)
+		public static async Task ForEachAsync<T>(this IEnumerable<T> items, Func<T, Task> action, int maxDegreeOfParalellism)
 		{
 			var allTasks = new List<Task>();
-			var throttler = new SemaphoreSlim(initialCount: maxThreads);
+			var throttler = new SemaphoreSlim(initialCount: maxDegreeOfParalellism);
 			foreach (var item in items)
 			{
 				await throttler.WaitAsync();
