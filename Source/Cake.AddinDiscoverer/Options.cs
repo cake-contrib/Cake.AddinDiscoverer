7acdf1dbedb7cccef5aadddde6329323f27ac467
Jeremie Desautels <desautelsj@hotmail.com>
2018-05-12 11:37:42 -0400

    Change order of options to make the help screen easier to read and understand

--- Source/Cake.AddinDiscoverer/Options.cs
+++ Source/Cake.AddinDiscoverer/Options.cs
@@ -4,17 +4,23 @@ namespace Cake.AddinDiscoverer
 {
 	internal class Options
 	{
+		[Option('v', "cakeversion", Required = true, HelpText = "The recommended Cake version. e.g.: 0.26.0")]
+		public string RecommendedCakeVersion { get; set; }
+
+		[Option('t', "tempfolder", Required = false, HelpText = "Folder where temporary files (including reports) are saved.")]
+		public string TemporaryFolder { get; set; }
+
 		[Option('a', "addinname", Required = false, HelpText = "Name of the specific addin to be audited. If omitted, all addins are audited.")]
 		public string AddinName { get; set; }
-
-		[Option('c', "clearcache", Default = false, HelpText = "Clear the list of addins that was previously cached.")]
-		public bool ClearCache { get; set; }
 
 		[Option('u', "user", Required = false, HelpText = "Github username.")]
 		public string GithubUsername { get; set; }
 
 		[Option('p', "password", Required = false, HelpText = "Github password.")]
 		public string GithuPassword { get; set; }
+
+		[Option('c', "clearcache", Default = false, HelpText = "Clear the list of addins that was previously cached.")]
+		public bool ClearCache { get; set; }
 
 		[Option('i', "issue", Default = false, HelpText = "Create issue in Github repositories that do not meet recommendations.")]
 		public bool CreateGithubIssue { get; set; }
@@ -30,11 +36,5 @@ namespace Cake.AddinDiscoverer
 
 		[Option('r', "markdowntorepo", Default = false, HelpText = "Generate the Markdown report and commit to cake-contrib repo.")]
 		public bool MarkdownReportToRepo { get; set; }
-
-		[Option('t', "tempfolder", Required = false, HelpText = "Folder where temporary files (including reports) are saved.")]
-		public string TemporaryFolder { get; set; }
-
-		[Option('v', "cakeversion", Required = true, HelpText = "The recommended Cake version. e.g.: 0.26.0")]
-		public string RecommendedCakeVersion { get; set; }
 	}
 }
