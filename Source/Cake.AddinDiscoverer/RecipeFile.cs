ca7db326168dde84d71bbc5caa111adc5e5605d5
Jeremie Desautels <desautelsj@hotmail.com>
2018-07-16 17:40:18 -0400

    Fix bug causing `loaddependencies=true` to be lost when parsing .cake files

--- Source/Cake.AddinDiscoverer/RecipeFile.cs
+++ Source/Cake.AddinDiscoverer/RecipeFile.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Web;
 
 namespace Cake.AddinDiscoverer
 {
 	internal class RecipeFile
 	{
-		public static readonly Regex AddinReferenceRegex = new Regex(@"(?<lineprefix>.*?)(?<packageprefix>\#addin nuget:\?package=)(?<packagename>.*)(?<versionprefix>&version=)(?<packageversion>.*)", RegexOptions.Compiled | RegexOptions.Multiline);
+		public static readonly Regex AddinReferenceRegex = new Regex("(?<lineprefix>.*?)(?<packageprefix>\\#addin nuget:\\?)(?<referencestring>.*?(?=(?:\")|$))(?<linepostfix>.*)", RegexOptions.Compiled | RegexOptions.Multiline);
 
 		private IEnumerable<AddinReference> _addinReferences;
 
@@ -27,8 +29,10 @@ namespace Cake.AddinDiscoverer
 					var matchResults = RecipeFile.AddinReferenceRegex.Matches(Content);
 					foreach (Match match in matchResults)
 					{
-						var packageName = match.Groups["packagename"].Value;
-						var referencedVersion = match.Groups["packageversion"].Value;
+						var parameters = HttpUtility.ParseQueryString(match.Groups["referencestring"].Value);
+
+						var packageName = parameters["package"];
+						var referencedVersion = parameters["version"];
 
 						references.Add(new AddinReference()
 						{
@@ -62,15 +66,35 @@ namespace Cake.AddinDiscoverer
 		{
 			var updatedContent = AddinReferenceRegex.Replace(content, match =>
 			{
-				var referencedAddin = AddinReferences.Where(addin => addin.Name.Equals(match.Groups["packagename"].Value, StringComparison.OrdinalIgnoreCase));
+				var parameters = HttpUtility.ParseQueryString(match.Groups["referencestring"].Value);
+
+				// These are the supported parameters as documented here: https://cakebuild.net/docs/fundamentals/preprocessor-directives
+				var packageName = parameters["package"];
+				var referencedVersion = parameters["version"];
+				var loadDependencies = parameters["loaddependencies"];
+				var include = parameters["include"];
+				var exclude = parameters["exclude"];
+				var prerelease = parameters.AllKeys.Contains("prerelease");
+
+				var referencedAddin = AddinReferences.Where(addin => addin.Name.Equals(packageName, StringComparison.OrdinalIgnoreCase));
 				if (!referencedAddin.Any()) return match.Groups[0].Value;
 
 				var updatedVersion = getAddinVersion(referencedAddin.First());
 				if (string.IsNullOrEmpty(updatedVersion)) return match.Groups[0].Value;
+				if (referencedVersion == updatedVersion) return match.Groups[0].Value;
 
-				if (match.Groups["packageversion"].Value == updatedVersion) return match.Groups[0].Value;
+				var newContent = new StringBuilder();
+				newContent.Append(match.Groups["lineprefix"].Value);
+				newContent.Append(match.Groups["packageprefix"].Value);
+				newContent.AppendFormat("package={0}", packageName);
+				newContent.AppendFormat("&version={0}", updatedVersion);
+				if (!string.IsNullOrEmpty(loadDependencies)) newContent.AppendFormat("&loaddependencies={0}", loadDependencies);
+				if (!string.IsNullOrEmpty(include)) newContent.AppendFormat("&include={0}", include);
+				if (!string.IsNullOrEmpty(exclude)) newContent.AppendFormat("&exclude={0}", exclude);
+				if (prerelease) newContent.Append("&prerelease");
+				newContent.Append(match.Groups["linepostfix"].Value);
 
-				return $"{match.Groups["lineprefix"].Value}{match.Groups["packageprefix"].Value}{match.Groups["packagename"].Value}{match.Groups["versionprefix"].Value}{updatedVersion}";
+				return newContent.ToString();
 			});
 
 			return updatedContent;
