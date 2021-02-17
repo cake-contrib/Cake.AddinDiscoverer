d45a41e04c092e972f2380d880637c79aa8358bd
Jericho <desautelsj@hotmail.com>
2021-02-17 09:33:43 -0500

    Correctly analyze Addins that reference neither Cake.Core nor Cake.Common
    Resolves #175

--- Source/Cake.AddinDiscoverer/Models/AddinReference.cs
+++ Source/Cake.AddinDiscoverer/Models/AddinReference.cs
@@ -8,5 +8,20 @@ namespace Cake.AddinDiscoverer.Models
 		public string LatestVersionForCurrentCake { get; set; }
 
 		public string LatestVersionForLatestCake { get; set; }
+
+		public bool UpdatedForLatestCake
+		{
+			get
+			{
+				// If both values are null it means that this addin does not reference Cake at all (e.g.: Cake.Email.Common)
+				if (string.IsNullOrEmpty(this.LatestVersionForCurrentCake) && string.IsNullOrEmpty(this.LatestVersionForLatestCake)) return true;
+
+				// If this value is non-null it means that an updated version is available
+				if (!string.IsNullOrEmpty(this.LatestVersionForLatestCake)) return true;
+
+				// Otherwise, it hasn't been updated (or there is no new version of Cake that introduces breaking changes)
+				return false;
+			}
+		}
 	}
 }
