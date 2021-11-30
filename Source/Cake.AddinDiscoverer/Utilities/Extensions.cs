c4204a5261ff4e651d3e9e324cb7e07e37662762
Jericho <desautelsj@hotmail.com>
2021-11-30 14:59:34 -0500

    Extension method to determine if two unsorted sequences contain same elements

--- Source/Cake.AddinDiscoverer/Utilities/Extensions.cs
+++ Source/Cake.AddinDiscoverer/Utilities/Extensions.cs
@@ -379,6 +379,35 @@ namespace Cake.AddinDiscoverer
 			return yamlContent;
 		}
 
+		public static bool UnsortedSequencesEqual<T>(this IEnumerable<T> first, IEnumerable<T> second, IEqualityComparer<T> comparer)
+		{
+			if (first == null) throw new ArgumentNullException("first");
+			if (second == null) throw new ArgumentNullException("second");
+
+			var counts = new Dictionary<T, int>(comparer);
+
+			foreach (var i in first)
+			{
+				if (counts.TryGetValue(i, out int c))
+					counts[i] = c + 1;
+				else
+					counts[i] = 1;
+			}
+
+			foreach (var i in second)
+			{
+				if (!counts.TryGetValue(i, out int c))
+					return false;
+
+				if (c == 1)
+					counts.Remove(i);
+				else
+					counts[i] = c - 1;
+			}
+
+			return counts.Count == 0;
+		}
+
 		private static void CheckIsEnum<T>(bool withFlags)
 		{
 			if (!typeof(T).IsEnum)
