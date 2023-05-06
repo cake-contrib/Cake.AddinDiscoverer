50d61f6b400fc9be047c061a8c5183800bf70817
jericho <desautelsj@hotmail.com>
2023-05-06 11:41:57 -0400

    Load result of previous analysis from local drive if available

--- Source/Cake.AddinDiscoverer/Steps/LoadPreviousAnalysisStep.cs
+++ Source/Cake.AddinDiscoverer/Steps/LoadPreviousAnalysisStep.cs
@@ -17,20 +17,26 @@ namespace Cake.AddinDiscoverer.Steps
 
 		public async Task ExecuteAsync(DiscoveryContext context, TextWriter log, CancellationToken cancellationToken)
 		{
-			// Load the result of previous analysis
-			var contents = await context.GithubClient.Repository.Content.GetAllContents(Constants.CAKE_CONTRIB_REPO_OWNER, Constants.CAKE_CONTRIB_REPO_NAME, Path.GetFileName(context.AnalysisResultSaveLocation)).ConfigureAwait(false);
+			string previousAnalysisContent;
 
-			// The file is too large to be retrieved from the GitHub API. We must issue a HTTP GET to the download URL
-			var previousAnalysisContent = await context.HttpClient.GetStringAsync(contents[0].DownloadUrl).ConfigureAwait(false);
+			if (File.Exists(context.AnalysisResultSaveLocation))
+			{
+				previousAnalysisContent = await File.ReadAllTextAsync(context.AnalysisResultSaveLocation, cancellationToken).ConfigureAwait(false);
+			}
+			else
+			{
+				// Load the result of previous analysis
+				var contents = await context.GithubClient.Repository.Content.GetAllContents(Constants.CAKE_CONTRIB_REPO_OWNER, Constants.CAKE_CONTRIB_REPO_NAME, Path.GetFileName(context.AnalysisResultSaveLocation)).ConfigureAwait(false);
+
+				// The file is too large to be retrieved from the GitHub API. We must issue a HTTP GET to the download URL
+				previousAnalysisContent = await context.HttpClient.GetStringAsync(contents[0].DownloadUrl).ConfigureAwait(false);
+			}
 
 			// Deseralize the content of the previous analysis
 			if (!string.IsNullOrEmpty(previousAnalysisContent))
 			{
 				context.Addins = JsonSerializer.Deserialize<AddinMetadata[]>(previousAnalysisContent, default(JsonSerializerOptions));
 			}
-
-			context.Addins = context.Addins.Where(addin => !addin.Name.EqualsIgnoreCase("Cake.Aws.ElasticBeanstalk")).ToArray();
-			context.Addins = context.Addins.Where(addin => !addin.Name.EqualsIgnoreCase("Cake.GithubUtility")).ToArray();
 
 			// Load individual analysis results (which are present if the previous analysis was interrupted)
 			var deserializedAddins = Directory.GetFiles(context.AnalysisFolder, "*.json")
