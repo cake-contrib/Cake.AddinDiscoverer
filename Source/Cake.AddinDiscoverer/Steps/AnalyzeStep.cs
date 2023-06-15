c15bd2a671ad9f195da6565342aadf3e6f4bc1de
jericho <desautelsj@hotmail.com>
2023-06-15 11:13:43 -0400

    Improve how me match an addin with repo under the cake-contrib organization to account for the possibility of the repo name being different than the addin name
    Resolves #221

--- Source/Cake.AddinDiscoverer/Steps/AnalyzeStep.cs
+++ Source/Cake.AddinDiscoverer/Steps/AnalyzeStep.cs
@@ -562,6 +562,8 @@ namespace Cake.AddinDiscoverer.Steps
 			// Some addins were moved to the cake-contrib organization but the URL in their package metadata still
 			// points to the original repo. This step corrects the URL to ensure it points to the right repo
 			var repo = cakeContribRepositories.FirstOrDefault(r => r.Name.EqualsIgnoreCase(addin.Name));
+			if (!string.IsNullOrEmpty(addin.RepositoryName)) repo ??= cakeContribRepositories.FirstOrDefault(r => r.Name.EqualsIgnoreCase(addin.RepositoryName));
+
 			if (repo != null)
 			{
 				// Only overwrite GitHub and Bitbucket URLs and preserve custom URLs such as 'https://cakeissues.net/' for example.
